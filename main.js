3ec2501ffd3cd5cf5d31fad4f35bf165cdde86a4
Robert Grimm <3990077+apparebit@users.noreply.github.com>
2020-04-30 22:41:10 -0400

    Add command line flags, colorful output, and helpful comments

--- main.js
+++ main.js
@@ -1,12 +1,56 @@
-import { toWebAssembly, toWebAssemblyText } from './src/compile.js';
+import { EOL } from 'os';
+import { toWebAssembly, toWebAssemblyText } from './js/compile.js';
 
-async function main() {
-  const args = process.argv.splice(2);
-  const wat = toWebAssemblyText(args);
-  const wasm = await toWebAssembly(wat);
-  const { instance } = await WebAssembly.instantiate(wasm);
-  const result = instance.exports.compute(665, 1);
-  console.log(`rerun ${args.join(' ')} --> ${result}`);
+const { isTTY } = process.stdout;
+const SGR = {
+  BOLD1: isTTY ? '\x1b[1m' : '',
+  BOLD0: isTTY ? '\x1b[22m' : '',
+};
+
+/**
+ * Extract command line options. This function only recognizes `--name`
+ * and `--no-name` for setting the `name` option to `true` and `false`,
+ * respectively.
+ */
+function getOptions(args) {
+  let options = {};
+
+  for (
+    let index = 0;
+    index < args.length && args[index].startsWith('--');
+    index++
+  ) {
+    let key = args.shift().slice(2);
+    let value = true;
+
+    if (key === '') {
+      break;
+    } else if (key.startsWith('no-')) {
+      key = key.slice(3);
+      value = false;
+    }
+
+    options[key] = value;
+  }
+
+  return options;
 }
 
-main();
+/** Run this tool. */
+(async function main() {
+  // Separate options from rerun code.
+  let rerun = process.argv.splice(2);
+  const options = getOptions(rerun);
+  if (rerun.length === 0) rerun = ['665'];
+
+  // Compile rerun code to WASM via textual assembly.
+  const wat = toWebAssemblyText(rerun);
+  const wasm = await toWebAssembly(wat, options);
+
+  // Compile WASM to native code and run it.
+  const { instance } = await WebAssembly.instantiate(wasm);
+  const result = instance.exports.compute(665, 1);
+
+  // Print the result.
+  console.log(`${SGR.BOLD1}rerun ${rerun.join(' ')} --> ${result}${SGR.BOLD0}`);
+})();
